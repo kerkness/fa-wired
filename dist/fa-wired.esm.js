2edee183939736008b723048f7b7d2717ad9f761
kerkness <ryan@kerkness.ca>
2025-05-28 12:50:09 -0500

    rebuild

--- dist/fa-wired.esm.js
+++ dist/fa-wired.esm.js
@@ -10,7 +10,7 @@
  * https://github.com/kerkness/fa-wired
  */
 const VERSION = "1.0.0";
-const BUILD_DATE = "2025-05-28T17:47:38.826Z";
+const BUILD_DATE = "2025-05-28T17:50:00.129Z";
 const FaWired = {
   version: VERSION,
   buildDate: BUILD_DATE,
